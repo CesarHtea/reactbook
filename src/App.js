3dbf6df456c840b00ccde13b3d3d1f5239a534be
CesarHtea <chernandez_83@yahoo.com.mx>
2018-03-20 13:17:13 -0600

    All files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import Post from './components/Post'
+
+import './App.css';
+
+class App extends Component {
+  render() {
+      return <Post />
+  }
+}
+
+export default App;
